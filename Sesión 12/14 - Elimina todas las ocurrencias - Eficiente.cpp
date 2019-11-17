56c6e3f3db96faa083ffeb98b21bf36af32dccb7
David <david.c.chicharro@gmail.com>
2019-11-17 13:15:22 +0100

    Update 14 - Elimina todas las ocurrencias - Eficiente.cpp

--- Sesión 12/14 - Elimina todas las ocurrencias - Eficiente.cpp
+++ Sesión 12/14 - Elimina todas las ocurrencias - Eficiente.cpp
@@ -1,38 +1,37 @@
 /*
 Alumno: David Carrasco Chicharro
-D.N.I.: 15520228-N
-Grado en IngenierÌa Inform·tica
-Asignatura: Fundamentos de la ProgramaciÛn
-Curso: 1∫ 	Grupo: C		Subgrupo: C1
-Profesor TeorÌa: Juan Carlos Cubero
-Proferosa Pr·cticas: MarÌa del Carmen Pegalajar
+Grado en Ingenier√≠a Inform√°tica
+Asignatura: Fundamentos de la Programaci√≥n
+Curso: 1¬∫ 	Grupo: C		Subgrupo: C1
+Profesor Teor√≠a: Juan Carlos Cubero
+Proferosa Pr√°cticas: Mar√≠a del Carmen Pegalajar
 
-SesiÛn 12
-Ejercicio 14 - Elimina todas las ocurrencias de un car·cter. VersiÛn eficiente
+Sesi√≥n 12
+Ejercicio 14 - Elimina todas las ocurrencias de un car√°cter. Versi√≥n eficiente
 
 El algoritmo del ejercicio 11 es muy ineficiente
 ya que requiere trasladar muchas veces muchas posiciones (usa dos bucles anidados). Para comprobarlo, recupere el texto
 http://decsai. ugr.es/~carlos/FP/Quijote_con _ruido.txt
 
-En Èl aparecen numerosas ocurrencias del car·cter '~' (cÛdigo ASCII 126) como
-ruido en el texto y se quieren eliminar. Si aplica el algoritmo ineficiente, la ejecuciÛn
-puede tardar m·s de 12 minutos.
+En √©l aparecen numerosas ocurrencias del car√°cter '~' (c√≥digo ASCII 126) como
+ruido en el texto y se quieren eliminar. Si aplica el algoritmo ineficiente, la ejecuci√≥n
+puede tardar m√°s de 12 minutos.
 
 Para resolver eficientemente este problema se propone utilizar dos variables,
 posicion_lectura y posicion_escritura   que nos vayan indicando, en cada momento, 
-la componente que se est· leyendo y el sitio dÛnde tiene que escribirse. 
+la componente que se est√° leyendo y el sitio d√≥nde tiene que escribirse. 
 Por ejemplo, supongamos que en un determinado momento la variable
 posicion_lectura vale 6 y posicion_escritura 3. 
-Si la componente en la posiciÛn 6 es el car·cter a borrar, simplemente avanzaremos posicion_lectura.
+Si la componente en la posici√≥n 6 es el car√°cter a borrar, simplemente avanzaremos posicion_lectura.
 
-En caso contrario, la colocaremos en la posiciÛn 3 y avanzaremos una posiciÛn ambas variables.
+En caso contrario, la colocaremos en la posici√≥n 3 y avanzaremos una posici√≥n ambas variables.
 
 Implemente este algoritmo y observe la diferencia de tiempo al ejecutarlo sobre el Quijote,
 
-ya que ahora el tiempo de ejecuciÛn es de unos 8 milisegundos.
+ya que ahora el tiempo de ejecuci√≥n es de unos 8 milisegundos.
 
-Como ampliaciÛn: si quiere saber con exactitud el tiempo de ejecuciÛn, puede utilizar
-un objeto de la clase Cronometro tal y como se vio en el ejercicio 24 de la relaciÛn de problemas III.
+Como ampliaci√≥n: si quiere saber con exactitud el tiempo de ejecuci√≥n, puede utilizar
+un objeto de la clase Cronometro tal y como se vio en el ejercicio 24 de la relaci√≥n de problemas III.
 */
 
 
