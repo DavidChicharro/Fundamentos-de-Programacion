053197bbe792e80375a0ab170869dc64622f2575
David <david.c.chicharro@gmail.com>
2019-11-17 13:15:12 +0100

    Update 13

--- Sesión 12/13 - Elimina bloque caracteres - Eficiente.cpp
+++ Sesión 12/13 - Elimina bloque caracteres - Eficiente.cpp
@@ -1,23 +1,22 @@
 /*
 Alumno: David Carrasco Chicharro
-D.N.I.: 15520228-N
-Grado en Ingeniería Informática
-Asignatura: Fundamentos de la Programación
-Curso: 1º 	Grupo: C		Subgrupo: C1
-Profesor Teoría: Juan Carlos Cubero
-Proferosa Prácticas: María del Carmen Pegalajar
+Grado en IngenierÃ­a InformÃ¡tica
+Asignatura: Fundamentos de la ProgramaciÃ³n
+Curso: 1Âº 	Grupo: C		Subgrupo: C1
+Profesor TeorÃ­a: Juan Carlos Cubero
+Proferosa PrÃ¡cticas: MarÃ­a del Carmen Pegalajar
 
-Sesión 12
-Ejercicio 13 - Elimina un bloque de caracteres. Versión eficiente
+SesiÃ³n 12
+Ejercicio 13 - Elimina un bloque de caracteres. VersiÃ³n eficiente
 
 Para borrar el bloque entre izda y dcha:
 
 	Calcular num_a_borrar como dcha - izda + 1
 		Recorrer las componentes -i- de la secuencia	entre las posiciones dcha+1 hasta el final
-			Colocar la componente -i- en la posición i-num_a_borrar
+			Colocar la componente -i- en la posiciÃ³n i-num_a_borrar
 		Actualizar total_utilizados
 		
-Este algoritmo resuelve el problema con un único bucle mientras que la versión ineficiente recurría a dos bucles anidados
+Este algoritmo resuelve el problema con un Ãºnico bucle mientras que la versiÃ³n ineficiente recurrÃ­a a dos bucles anidados
 
 */
 
