475c2f9ab0bd42763836f34c55ecde215aa27494
David <david.c.chicharro@gmail.com>
2019-11-19 20:03:17 +0100

    Update S13 Ej18 - Elimina exceso de blancos.cpp

--- Sesión 12-13/S13 Ej18 - Elimina exceso de blancos.cpp
+++ Sesión 12-13/S13 Ej18 - Elimina exceso de blancos.cpp
@@ -1,17 +1,16 @@
 /*
 Alumno: David Carrasco Chicharro
-D.N.I.: 15520228-N
-Grado en IngenierÌa Inform·tica
-Asignatura: Fundamentos de la ProgramaciÛn
-Curso: 1∫ 	Grupo: C		Subgrupo: C1
-Profesor TeorÌa: Juan Carlos Cubero
-Proferosa Pr·cticas: MarÌa del Carmen Pegalajar
+Grado en Ingenier√≠a Inform√°tica
+Asignatura: Fundamentos de la Programaci√≥n
+Curso: 1¬∫ 	Grupo: C		Subgrupo: C1
+Profesor Teor√≠a: Juan Carlos Cubero
+Proferosa Pr√°cticas: Mar√≠a del Carmen Pegalajar
 
-SesiÛn 13
+Sesi√≥n 13
 Ejercicio 18 - Elimina exceso de blancos 
 
 Programa que lee una secuencia de caracteres y elimina todos los espacios en blanco
-sobrantes siempre que haya dos o m·s espacios, dejando sÛlo uno. Finalmente mostrar·
+sobrantes siempre que haya dos o m√°s espacios, dejando s√≥lo uno. Finalmente mostrar√°
 el resultado en pantalla con la misma secuencia de entrada pero sin los espacios sobrantes.
 
 */
@@ -53,7 +52,7 @@ public:
 		for (int i = 0 ; i < total_utilizados ; i++)
 			if (vector_privado[i] == ' ' && vector_privado[i+1] == ' ')  		// Cuando el caracter actual y el siguiente sean espacios en blanco
 				while (vector_privado[i+1] == ' '){										// Mientras que el caracter siguiente a un espacio sea otro espacio
-					for (int j = i ; j < total_utilizados ; j++)						// Se desplazar·n todos los caracteres una posiciÛn a la izquierda
+					for (int j = i ; j < total_utilizados ; j++)						// Se desplazar√°n todos los caracteres una posici√≥n a la izquierda
 						vector_privado[j] = vector_privado[j+1];						
 					
 					total_utilizados--;
@@ -75,7 +74,7 @@ int main(){
 		cin.get(caracter);
 	}
 	
-	// CÛmputos
+	// C√≥mputos
 	secuencia.EliminaExcesoBlancos();
 	
 	// Salida de resultados
